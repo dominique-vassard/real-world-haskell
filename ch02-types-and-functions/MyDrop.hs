3444728ae9f7ccfc4b90eb9663d73d43d0786a0e
Dominique VASSARD <dominique.vassard@kwanko.com>
2018-01-11 22:03:15 +0100

    Update chapter 2 code

---
+++ ch02-types-and-functions/MyDrop.hs
@@ -0,0 +1,14 @@
+module MyDrop(myDrop, niceDrop) where
+
+myDrop :: Int -> [a] -> [a]
+myDrop n xs =
+  if n <= 0 || null xs then
+    xs
+  else
+    myDrop (n-1) (tail xs)
+
+niceDrop :: Int -> [a] -> [a]
+niceDrop n xs
+  | n <= 0 = xs
+niceDrop _ [] = []
+niceDrop n (_:xs) = niceDrop (n-1) xs
