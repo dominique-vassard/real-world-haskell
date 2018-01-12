d3923c2fc7bbb456894c48d63e4d06666e05d81d
Dominique VASSARD <dominique.vassard@kwanko.com>
2018-01-12 18:07:24 +0100

    On step furthr in th exercises

--- ch03-defining-types-streamlining-functions/Exercises.hs
+++ ch03-defining-types-streamlining-functions/Exercises.hs
@@ -1,13 +1,13 @@
 
-module Exercises(length', sum', mean, reverse', toPalindrome, isPalindrome, sortList, intersperse') where
+module Exercises(length', sum', mean, reverse', toPalindrome, isPalindrome, sortList, intersperse', height) where
 import qualified Data.List as L
 
 length' :: [a] -> Int
-length' [] = 0
+length' []     = 0
 length' (_:xs) = 1 + length' xs
 
 sum' :: (Num a) => [a] -> a
-sum' [] = 0
+sum' []     = 0
 sum' (x:xs) = x + sum' xs
 
 mean :: Fractional p => [p] -> p
@@ -15,7 +15,7 @@ mean [] = 0
 mean xs = sum' xs / fromIntegral (length' xs)
 
 reverse' :: [a] -> [a]
-reverse' [] = []
+reverse' []     = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
 toPalindrome :: [a] -> [a]
@@ -29,6 +29,29 @@ sortList :: [[a]] -> [[a]]
 sortList = L.sortBy (\a b -> compare (length' a) (length' b))
 
 intersperse' :: String -> [String] -> String
-intersperse' _ [] = []
-intersperse' _ [x] = x
+intersperse' _ []     = []
+intersperse' _ [x]    = x
 intersperse' s (x:xs) = x ++ s ++ intersperse' s xs
+
+
+-- Using the binary tree type that we defined earlier in this chapter, write a function that will determine the height of the tree.
+-- The height is the largest number of hops from the root to an Empty.
+-- For example, the tree Empty has height zero; Node "x" Empty Empty has height one; Node "x" Empty (Node "y" Empty Empty) has height two; and so on.
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+            deriving (Show)
+
+height :: Tree a -> Int
+height Empty          = 0
+height (Node _ lt rt) = 1 + max (height lt)  (height rt)
+
+-- Consider three two-dimensional points a, b, and c.
+-- If we look at the angle formed by the line segment from a to b and the line segment from b to c, it either turns left, turns right, or forms a straight line.
+-- Define a Direction data type that lets you represent these possibilities.
+-- Write a function that calculates the turn made by three 2D points and returns a Direction.
+data Point = Point (Int, Int)
+data Direction = Left
+                | Straight
+                | Right
+
+-- getDirection :: [a] -> Direction
