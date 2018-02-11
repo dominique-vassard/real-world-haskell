d0c857a0829634351d9fb65d4ddc27750aeb594f
Dominique VASSARD <dominique.vassard@kwanko.com>
2018-02-11 12:47:34 +0100

    ch04 - end fold

--- ch04-functional-programming/ExercisesFold.hs
+++ ch04-functional-programming/ExercisesFold.hs
@@ -1,6 +1,9 @@
 module ExercisesFold
-  ( groupBy', concat'
+  ( groupBy', concat', takeWhile', takeWhileFold, any', words', unlines'
   ) where
+
+
+import Data.List (foldl')
 
 -- The Data.List module defines a function, groupBy, which has the following type.
 -- Use ghci to load the Data.List module and figure out what groupBy does, then write your own implementation using a fold.
@@ -21,3 +24,39 @@ groupBy' f =
 -- The Prelude function concat concatenates a list of lists into a single list, and has the following type.
 concat' :: [[a]] -> [a]
 concat' = foldr (++) []
+
+-- How many of the following Prelude functions can you rewrite using list folds?
+--   - any
+--    Yes. Better is foldr to allow infinite list treatment
+--   - cycle
+--    No, can't product infinite list from finite list with only a fold
+--   - words
+--    Yes. foldl' to use minimum memory (chunk can be quite large)
+--   - unlines
+--    Yes. foldr for inifinte list
+-- For those functions where you can use either foldl' or foldr, which is more appropriate in each case?
+
+any' :: (a -> Bool) -> [a] -> Bool
+any' f = foldl (\acc a -> acc || f a) False
+
+words' :: String -> [String]
+words' s = reverse $ foldl' f [""] s
+  where
+    f [] _ = []
+    f acc ' ' = []:acc
+    f (x:xs) l = (x ++ [l]):xs
+
+unlines' :: [String] -> String
+unlines' = foldr (\x acc -> x ++ "\n" ++ acc) ""
+
+-- Write your own definition of the standard takeWhile function, first using explicit recursion, then foldr.
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' f = loop []
+  where
+    loop acc [] = reverse acc
+    loop acc (x:xs)
+      | f x = loop (x:acc) xs
+      | otherwise = loop acc xs
+
+takeWhileFold :: (a -> Bool) -> [a] -> [a]
+takeWhileFold f = foldr (\a acc -> if f a then a:acc else []) []
